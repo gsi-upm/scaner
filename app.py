b431e0785fd88487f6149997f9b281e6ef1c79b6
J. Fernando Sánchez <balkian@gmail.com>
2016-02-08 16:29:46 +0100

    Added redirect

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ import connexion
 import logging
 import os
 from connexion.resolver import RestyResolver
+from flask import redirect
 
 logging.basicConfig(level=logging.INFO)
+
+def go_to_ui():
+    return redirect('/api/v1/ui')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app = connexion.App(__name__)
+    app.app.add_url_rule('/', 'index', go_to_ui)
     app.add_api('scaner_api.yaml', arguments={'title': 'Scaner\'s API'}, resolver=RestyResolver('scaner.controllers'))
     app.run(port=port)
